b4b42e11332b72116161c980eebba7feb5d4a79c
Johan Gavelan <johangavelan@gmail.com>
2021-04-19 02:21:59 -0500

    service section done

--- front-page.php
+++ front-page.php
@@ -2,5 +2,6 @@
 
 <?php get_template_part('includes/section','newsletter'); ?>
 <?php get_template_part('includes/section','who'); ?>
+<?php get_template_part('includes/section','service'); ?>
 
 <?php get_footer(); ?>
---
+++ includes/section-service.php
@@ -0,0 +1,10 @@
+<div class="service">
+  <div class="content-wrapper">
+    <h2 class="sub-heading light">What do we do for you</h2>
+    <div class="border-gradient"></div>
+    <p>We provide you with a source of prospects who need your help and a coherent system to efficiently turn them into clients. By outsourcing your prospecting efforts to us for a low monthly flat fee, your brilliant mind is free to keep you and your clients happy.</p>
+    <p>Through studying tons of successful Ad campaigns we know what strategies work, but most importantly we know what doesn’t and we’ve learned how to follow the guidelines of varying degrees of Compliance.</p>
+    <p>We know that your Return on Investment with us will determine whether you continue to work with us year after year, and although we are using proven methods we also believe in minimizing your risk. So, to serve your best interests, we will run a Facebook Ad for your business for Free until we bring you leads. That sounds too good to be true, because we still need you to pay Facebook for using their Ad platform, however our monthly service fee will be 100% off until we provide you with your first prospects. Click here if you’d like to read more about our <strong>Risk Aversion Policy.</strong></p>
+    <p><em>*Financial Advisors we choose to work with must uphold Fiduciary Duties.</em></p>
+  </div>
+</div>
