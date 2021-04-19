574ccb88ff4321590ecf6741331bddcdec884f13
Johan Gavelan <johangavelan@gmail.com>
2021-04-19 08:54:33 -0500

    process section done

--- front-page.php
+++ front-page.php
@@ -3,5 +3,6 @@
 <?php get_template_part('includes/section','newsletter'); ?>
 <?php get_template_part('includes/section','who'); ?>
 <?php get_template_part('includes/section','service'); ?>
+<?php get_template_part('includes/section','process'); ?>
 
 <?php get_footer(); ?>
---
+++ includes/section-process.php
@@ -0,0 +1,68 @@
+<?php    
+    $upload_dir = wp_upload_dir();
+?>
+
+<div class="process">
+  <div class="content-wrapper">
+    <div class="process__heading">
+      <h2 class="sub-heading dark">How to work with us?</h2>
+      <div class="border-gradient"></div>
+    </div>
+    <div class="grid-row col-5">
+      <!-- step 1 -->
+      <div class="process__step">
+        <h3>15-minute Strategy Call</h3>
+        <img src="<?=$upload_dir['baseurl'];?>/2021/04/first-call.png" alt="mobile icon">
+        <p>By the end of this call you will have a completely free and personalized online strategy that you can implement with or without us. No strings attached.</p>
+      </div>
+      <!-- step 2 -->
+      <div class="process__step">
+        <h3>45-minute Onboarding Call</h3>
+        <img src="<?=$upload_dir['baseurl'];?>/2021/04/analytics.png" alt="analytics icon">
+        <p>If we are a good fit for each other this is where we will gather additional info to implement your strategy.</p>
+      </div>
+      <!-- step 3 -->
+      <div class="process__step">
+        <h3>Compliance</h3>
+        <img src="<?=$upload_dir['baseurl'];?>/2021/04/compliance.png" alt="compliance icon">
+        <p>Within 2 weeks after receiving all necessary info, your Ad will be created and submitted through your designated Compliance portal.</p>
+      </div>
+      <!-- step 4 -->
+      <div class="process__step">
+        <h3>Approval</h3>
+        <img src="<?=$upload_dir['baseurl'];?>/2021/04/approval.png" alt="approval icon">
+        <p>This is when we turn on your Ad and begin tracking metrics.</p>
+      </div>
+      <!-- step 5 -->
+      <div class="process__step">
+        <h3>Optimization</h3>
+        <img src="<?=$upload_dir['baseurl'];?>/2021/04/statistics.png" alt="statistics icon">
+        <p>We will fine tune the targeting of your ad to provide higher quality results.</p>
+      </div>
+    </div>
+    <!-- step numbers -->
+    <div class="process__step-numbers grid-row col-5 mt-3">
+      <!-- step -->
+      <div class="process__step-number">
+        <span>1</span>
+      </div>
+      <!-- step -->
+      <div class="process__step-number">
+        <span>2</span>
+      </div>
+      <!-- step -->
+      <div class="process__step-number">
+        <span>3</span>
+      </div>
+      <!-- step -->
+      <div class="process__step-number">
+        <span>4</span>
+      </div>
+      <!-- step -->
+      <div class="process__step-number">
+        <span>5</span>
+      </div>
+    </div>
+  </div>
+  
+</div>
