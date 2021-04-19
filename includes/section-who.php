df5dc82de90bbda3cf4bcae23402f7d16834d55c
Johan Gavelan <johangavelan@gmail.com>
2021-04-19 02:07:12 -0500

    who section done

--- includes/section-who.php
+++ includes/section-who.php
@@ -1,7 +1,7 @@
 <div class="who">
   <div class="content-wrapper">
     <h2 class="sub-heading dark">Who are we?</h2>
-    <span class="gradient-border"></span>
+    <div class="border-gradient"></div>
     <p>Marketing geniuses that focus solely within the financial industry to help financial advisors*, wealth managers, and retirement planners tap into an unyielding source of potential clients.</p>
     <p>We are quick to adapt to the dynamic nature of Digital Marketing Strategies so just like you know how to manage your clients’ assets, we know how to bring you more qualified clients.</p>
   </div>
